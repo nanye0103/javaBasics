165e1cc26c11a86cbacd32e50cf8d70e91b6c051
nanye0103 <wangleilei0103@163.com>
2020-05-04 13:41:01 +0800

    第一次提交，新建一个hello world

---
+++ basicgrammar/src/main/java/com/wll/java/basicgrammer/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.wll.java.basicgrammer;
+
+/**
+ * @Author: wangleilei
+ * @Date: 2020-05-04 13:28
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("hello world!!!");
+    }
+}
